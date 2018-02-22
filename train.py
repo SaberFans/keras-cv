3e025bc7d4a6e76b997a42fee53bbda3db6f15c3
tianyang926 <tianyang926@gmail.com>
2018-02-22 19:45:59 +0000

    try

--- train.py
+++ train.py
@@ -34,20 +34,20 @@ img_width, img_height = 64, 64
 def create_lava_model(input_shape):
     model = Sequential([
         # first conv layer
-        Conv2D(96, kernel_size=(3, 3), strides=(2, 2), input_shape=input_shape,
+        Conv2D(96, kernel_size=3, strides=(2, 2), input_shape=input_shape,
                activation='relu'),
         MaxPooling2D(pool_size=(3, 3), strides=(2, 2)),
         BatchNormalization(),
 
         # second conv layer
-        Conv2D(256, kernel_size=(3, 3), activation='relu'),
+        Conv2D(256, kernel_size=3, activation='relu'),
         MaxPooling2D(pool_size=(3, 3), strides=(2, 2)),
         BatchNormalization(),
 
         # 3-5 conv layers
-        Conv2D(384, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='valid'),
-        Conv2D(384, kerner_size=(3, 3), strides=(1, 1), activation='relu', padding='valid'),
-        Conv2D(256, kerner_size=(3, 3), strides=(1, 1), activation='relu', padding='valid'),
+        Conv2D(384, kernel_size=3, strides=(1, 1), activation='relu', padding='valid'),
+        Conv2D(384, kerner_size=3, strides=(1, 1), activation='relu', padding='valid'),
+        Conv2D(256, kerner_size=3, strides=(1, 1), activation='relu', padding='valid'),
         MaxPooling2D(pool_size=(3, 3), strides=(2, 2)),
         BatchNormalization(),
         # Fully connected layer
