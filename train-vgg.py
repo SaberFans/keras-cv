0fc0874c67a8f4ed26b8369dac401247ac9518b7
tianyang926 <tianyang926@gmail.com>
2018-02-22 04:40:15 +0000

    recognize file

--- train-vgg.py
+++ train-vgg.py
@@ -40,21 +40,21 @@ def main(data_dir, model_name):
     # Let's train the model using RMSprop
     vgg_model.compile(loss='categorical_crossentropy',
                   optimizer=opt,
-                  metrics=['accuracy', top_5_accuracy])
+                  metrics=['accuracy'])
 
     print('Using real-time data augmentation.')
     # This will do preprocessing and realtime data augmentation:
     train_datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the datasets
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the datasets
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip=True,  # randomly flip images
-        vertical_flip=False,  # randomly flip images
+        # featurewise_center=False,  # set input mean to 0 over the datasets
+        # samplewise_center=False,  # set each sample mean to 0
+        # featurewise_std_normalization=False,  # divide inputs by std of the datasets
+        # samplewise_std_normalization=False,  # divide each input by its std
+        # zca_whitening=False,  # apply ZCA whitening
+        # rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+        # width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+        # height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+        # horizontal_flip=True,  # randomly flip images
+        # vertical_flip=False,  # randomly flip images
         rescale=1. / 255)  # normalize the grb value
     test_datagen = ImageDataGenerator(rescale=1. / 255)
 
@@ -62,12 +62,14 @@ def main(data_dir, model_name):
         data_dir + '/train',
         target_size=(img_width, img_height),
         batch_size=batch_size,
+        save_format='JPEG',
         class_mode='categorical')
 
     validation_generator = test_datagen.flow_from_directory(
         data_dir + '/val',
         target_size=(img_width, img_height),
         batch_size=batch_size,
+        save_format='JPEG',
         class_mode='categorical')
 
     # Compute quantities required for feature-wise normalization
