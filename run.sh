7893d576d8cddbce5b6b207d9aa83a05f696b8b7
tianyang926 <tianyang926@gmail.com>
2018-02-25 03:24:03 +0000

    modify run script for pretrain

--- run.sh
+++ run.sh
@@ -4,8 +4,16 @@ python train-res50.py
 echo "========================================"
 
 echo "=======Training vgg 16 started======"
-python train-vgg.py
+python train-xception.py
 echo "========================================"
 
 echo "=======Training miniCnn started======"
 python train-miniCnn.py
+
+echo "=======Pre-Training res 50 started======"
+python train-res50.py --pretrain yes --name res50pretrain
+echo "========================================"
+
+echo "=======Training vgg 16 started======"
+python train-xception.py --pretrain yes --name xceptionpretrain
+echo "========================================"
